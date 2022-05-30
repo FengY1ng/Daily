56821442a08bb84127199edac540d91298026cf9
FengY1ng <DrFengY1ng@gmail.com>
2022-05-30 14:37:46 +0800

    daily

--- Daily/Daily/Daily.cpp
+++ Daily/Daily/Daily.cpp
@@ -1,22 +1,25 @@
 ﻿#include <iostream>
 #include <string>
 #include <conio.h>
+#include <cstring>
 
 using namespace std;
 
-class Student
-{
-private:
-	char ch[10] = "菜鸡";
-	int score = 18;
-public:
-
-};
-
 int main()
 {
-	
-	cout << ch2;
+	int a[10] = { 17,12,5,36,55,19,3,9,25,41 },*p1=a;
+	for (int i = 0; i < 10; i++)
+	{
+		int *max = p1+i;
+		for (int j = i; j < 10; j++)
+			if (*(p1+j) > *max)
+				max = p1+j;
+		int t = *max;
+		*max = *(p1+i);
+		*(p1+i) = t;
+	}
+	for (int i = 0; i < 10; i++)
+		cout << a[i]<<"\t";
 }
 
 //// Project05.cpp : Defines the entry point for the console application.
