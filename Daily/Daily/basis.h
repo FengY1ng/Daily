7c279e74f32e01ea84c82159c88c7547f184f01c
DrFengY1ng@gmail.com <DrFengY1ng@gmail.com>
2022-08-25 16:06:08 +0800

    nothing

---
+++ Daily/Daily/basis.h
@@ -0,0 +1,14 @@
+#pragma once
+//C++
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+//C
+#include <conio.h>
+
+using namespace std;
