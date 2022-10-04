58ef55c0baa18f70d08db04d2c60d18124fc5a9e
DrFengY1ng@gmail.com <DrFengY1ng@gmail.com>
2022-10-04 20:14:59 +0800

    nothing

---
+++ Daily_Java/src/Daliy1/experiment_3_1.java
@@ -0,0 +1,13 @@
+package Daliy1;
+
+public class experiment_3_1 {
+    static class Person
+    {
+        private String[] name;
+        private int age;
+        private char sex;
+        public Person(){
+
+        }
+    }
+}
