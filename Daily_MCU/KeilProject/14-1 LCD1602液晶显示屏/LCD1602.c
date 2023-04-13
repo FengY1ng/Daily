97a83be9d75a7fd79df6d8d8afa9e4d993ad59fd
DrFengY1ng@gmail.com <DrFengY1ng@gmail.com>
2023-04-13 18:29:28 +0800

    Add LCD1602 Folder

---
+++ Daily_MCU/KeilProject/14-1 LCD1602液晶显示屏/LCD1602.c
@@ -0,0 +1,21 @@
+#include <REGX52.H>
+
+void LCD1602_Init(void)
+{
+
+}
+
+void LCD1602_WriteString(unsigned char col,unsigned char raw)
+{
+
+}
+
+void LCD1602_WriteNum(unsigned char col,unsigned char raw,)
+{
+
+}
+
+void LCD1602_WirteSignedNum(unsigned char col,unsigned char raw,)
+{
+    
+}
