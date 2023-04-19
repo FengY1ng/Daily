74fda252d4876b951bd2942935ba21c1dbcd4110
DrFengY1ng@gmail.com <DrFengY1ng@gmail.com>
2023-04-19 22:07:51 +0800

    Add IR(bug)

--- Daily_MCU/KeilProject/17-1 红外遥控/main.c
+++ Daily_MCU/KeilProject/17-1 红外遥控/main.c
@@ -1,10 +1,27 @@
 #include <REGX52.H>
+#include "Delay.h"
+#include "LCD1602.h"
+#include "IR.h"
+
+unsigned char Num;
+unsigned char Address,Command;
 
 void main()
 {
-    
+    LCD_Init();
+    LCD_ShowString(1,1,"A");
+
+    IR_Init();
+
     while(1)
     {
-        
+        if(IR_GetDataFlag())
+        {
+            Address=IR_GetAddress();
+            Command=IR_GetCommand();
+
+            LCD_ShowHexNum(2,1,Address,2);
+            LCD_ShowHexNum(2,5,Command,2);
+        }
     }
 }
