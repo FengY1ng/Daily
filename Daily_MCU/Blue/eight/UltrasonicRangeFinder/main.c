3b60f0c4320792cc66515e6f3420dc0a4370e4b1
DrFengY1ng@gmail.com <DrFengY1ng@gmail.com>
2023-04-26 12:04:25 +0800

    Add Timer0 Folder

--- Daily_MCU/Blue/eight/UltrasonicRangeFinder/main.c
+++ Daily_MCU/Blue/eight/UltrasonicRangeFinder/main.c
@@ -1,8 +1,10 @@
 #include <REGX52.H>
 #include "Nixie.h"
+#include "Timer0.h"
 
-void ProcessInit()
+void main()
 {
+    Timer0_Init();  
     NixieShowOwn(0,0x39);
     NixieShowNum(2,0);
     NixieShowNum(3,3);
@@ -10,13 +12,20 @@ void ProcessInit()
     NixieShowNum(5,0);
     NixieShowNum(6,4);
     NixieShowNum(7,0);
-}
-
-void main()
-{
-    NixieShowNum(5,7);
     while(1)
     {
-        
+        NixieShowNum(1,4);
     }
+}
+
+void Timer0_Routine() interrupt 1
+{
+	static unsigned int T0Count;
+	TL0 = 0x66;				//设置定时初始值
+	TH0 = 0xFC;				//设置定时初始值
+	T0Count++;
+	if(T0Count>=1000)
+	{
+		T0Count=0;
+	}
 }
