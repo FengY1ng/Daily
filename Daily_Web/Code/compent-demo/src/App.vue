1fe05a8f35a7255943a9b2b160aedeba7026b231
DrFengY1ng@gmail.com <DrFengY1ng@gmail.com>
2023-06-26 22:00:47 +0800

    learn Vue

---
+++ Daily_Web/Code/compent-demo/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Movie v-for="movie in movies" :key="movie.id" :title="movie.title" :rating="movie.rating"></Movie>
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+import Movie from './components/Movie.vue'
+import Hello from './components/Hello.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Movie,
+    Hello
+  },
+    data:function(){
+      return {
+        movies:[
+          {id:1,title:"金刚狼1",rating:"8.7"},
+          {id:2,title:"金刚狼2",rating:"8.7"},
+          {id:3,title:"金刚狼3",rating:"8.7"},
+          {id:4,title:"金刚狼4",rating:"8.7"}
+        ]
+      }
+    },
+    
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Daily_Web/Code/compent-demo/src/components/Movie.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <span>{{ rating }}</span>
+        <button @click="fun">点击收藏</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Movie',
+    props:["title","rating"],
+    data: function () {
+        return {
+            
+        }
+    },
+    methods:{
+        fun:function(){
+            alert("收藏成功");
+        }
+    }
+}
+</script>
